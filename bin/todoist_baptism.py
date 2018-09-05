a3afacc171dfc1b9381554266330d9adc12c58ff
Wade Allen <wadesallen@gmail.com>
2018-09-05 17:27:30 +0000

    updated site

--- bin/todoist_baptism.py
+++ bin/todoist_baptism.py
@@ -8,7 +8,6 @@ import datetime
 import config
 import sys
 import subprocess
-import urltools
 import argparse
 
 parser = argparse.ArgumentParser(description='Set up Baptism Template')
@@ -55,9 +54,4 @@ database_date = database_date + datetime.timedelta(days=+1)
 item = api.items.add('Add ' + name + ' baptism information to database', project_id, date_string=database_date.strftime('%Y-%m-%d'))
 api.commit()
 
-# email_address = 'clif@fbcmuncie.org'
-# subject = 'Baptism on {}'.format(date.strftime('%B %d, %Y'))
-# message = ('Hi Clif%0A%0AI just wanted you to know that {} is going to be baptized on {}. I have added it to planning center.%0A%0AWade'.format(name, date.strftime('%B %d, %Y')))
-# url = 'https://mail.google.com/mail/?view=cm&fs=1&to=' + email_address + '&su=' + subject + '&body=' + message
-# subprocess.call(["xdg-open", url])
 
