32d0cab804b5e5b972932384549ecc0041a88e50
Wade Allen <wadesallen@gmail.com>
2018-09-06 08:45:26 -0400

    updated site

--- bin/baby-dedication.py
+++ bin/baby-dedication.py
@@ -4,6 +4,17 @@ import todoist
 import datetime
 import config
 import argparse
+from os.path import expanduser
+import os
+import os.path
+
+def suffix(d):
+    return 'th' if 11<=d<=13 else {1:'st',2:'nd',3:'rd'}.get(d%10, 'th')
+
+def custom_strftime(format, t):
+    return t.strftime(format).replace('{S}', str(t.day) + suffix(t.day))
+
+home = expanduser("~")
 
 parser = argparse.ArgumentParser(description='Set up Baby Dedication Template')
 parser.add_argument('-n','--name', nargs='*', help='Name',  required=True)
@@ -25,10 +36,33 @@ api = todoist.TodoistAPI(config.todoist_api)
 
 # make baby dedication certificate
 certificate_date = set_date(-4)
-item = api.items.add('Make ' + name + ' dedication certificate ' + date, project_id, date_string=certificate_date.strftime('%Y-%m-%d'))
+item = api.items.add('Print ' + name + ' dedication certificate ' + date, project_id, date_string=certificate_date.strftime('%Y-%m-%d'))
 
 # make baby dedication certificate
 pc_date = set_date(-15)
 item = api.items.add('Add ' + name + ' dedication in Planning Center ' + date, project_id, date_string=pc_date.strftime('%Y-%m-%d'))
 api.commit()
+
+# Compile baby dedication certificate
+dedication_date = datetime.datetime.strptime(date, '%Y-%m-%d') 
+day = dedication_date.strftime('%d')
+day = custom_strftime('{S}', dedication_date)
+month = dedication_date.strftime('%B')
+year = dedication_date.strftime('%Y')
+
+filename =(home + '/Dropbox/Administration/Certificates/Baby_Dedication/Dedications/' + date + "_" + slug + '.md')
+
+if os.path.isfile(filename):
+	print("File already exists")
+else:
+    target = open (filename, 'w')
+    target.write("---\n")
+    target.write("name: " + name + "\n")
+    target.write("day: " + day + "\n")
+    target.write("month: " + month + "\n")
+    target.write("year: " + year + "\n")
+    target.write("---\n")
+    target.close()
+
+os.system('pandoc ' + filename + ' -o ~/Dropbox/Print/' + date + '_' + slug + '.pdf --latex-engine=xelatex --template=baby_dedication.tex')
 
