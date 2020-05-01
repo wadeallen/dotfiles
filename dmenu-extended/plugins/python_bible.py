f94ffb65b9f7b4c82cb7b68f214ec0164ddf6119
Wade Allen <wadesallen@gmail.com>
2020-05-01 00:02:23 -0400

    updated site

---
+++ dmenu-extended/plugins/python_bible.py
@@ -0,0 +1,25 @@
+import dmenu_extended
+import sys
+
+file = '/home/wadeallen/dotfiles/dmenu-extended/bible_books.json'
+
+class extension(dmenu_extended.dmenu):
+
+    # Set the name to appear in the menu
+    title = 'Bible'
+
+    # Determines whether to attach the submenu indicator
+    is_submenu = True
+
+
+    # Required function, runs when the user fires the menu item
+    def run(self, inputText):
+    	books_json = self.load_json(file)
+    	books =[]
+    	for b in books_json['sections']:
+    		books.append(b['key'])
+
+    	passage = self.menu(books, prompt='Enter book')
+    	passage = passage.replace(" ", "+")
+    	verse = self.menu('', prompt='Enter Chapter/Verse')
+    	self.open_url(f"https://netbible.org/bible/{passage}+{verse}")
