5419190a3ca701a247a645075c93274092cdaf40
Wade Allen <wadesallen@gmail.com>
2020-06-18 18:37:12 -0400

    updated site

---
+++ bin/last_day_month.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+import datetime
+
+today = datetime.date.today()
+first = today.replace(day=1)
+lastMonth = first - datetime.timedelta(days=1)
+print(lastMonth.strftime("%Y-%m-%d"))
