6f4297f883c282a164372e7bc07d22c7aaf1d9f0
Wade Allen <wadesallen@gmail.com>
2020-07-28 09:01:50 -0400

    updated site

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,7 +1,26 @@
-function fish_prompt --description 'Screen Savvy prompt'
-    if test -z "$WINDOW"
-        printf '%s%s@%s%s%s%s%s> ' (set_color yellow) $USER (set_color purple) (prompt_hostname) (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
-    else
-        printf '%s%s@%s%s%s(%s)%s%s%s> ' (set_color yellow) $USER (set_color purple) (prompt_hostname) (set_color white) (echo $WINDOW) (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
+function fish_prompt
+    if not set -q VIRTUAL_ENV_DISABLE_PROMPT
+        set -g VIRTUAL_ENV_DISABLE_PROMPT true
     end
+    set_color yellow
+    printf '%s' $USER
+    set_color normal
+    printf ' at '
+
+    set_color magenta
+    echo -n (prompt_hostname)
+    set_color normal
+    printf ' in '
+
+    set_color $fish_color_cwd
+    printf '%s' (prompt_pwd)
+    set_color normal
+
+    # Line 2
+    echo
+    if test $VIRTUAL_ENV
+        printf "(%s) " (set_color blue)(basename $VIRTUAL_ENV)(set_color normal)
+    end
+    printf '↪ '
+    set_color normal
 end
