10de940936104f8bd9a5301da8044f9856278918
Wade Allen <wadesallen@gmail.com>
2020-07-28 08:54:02 -0400

    updated site

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,18 +1,10 @@
-function fish_prompt --description 'Write out the prompt'
-    set -l color_cwd
-    set -l suffix
-    switch "$USER"
-        case root toor
-            if set -q fish_color_cwd_root
-                set color_cwd $fish_color_cwd_root
-            else
-                set color_cwd $fish_color_cwd
-            end
-            set suffix '#'
-        case '*'
-            set color_cwd $fish_color_cwd
-            set suffix '>'
-    end
-
-    echo -n -s (prompt_hostname) ' ' (set_color $color_cwd) (prompt_pwd) (set_color normal) "$suffix "
+function fish_prompt
+    set -l textcol  white
+    set -l arrowcol green
+    set -l filebase yellow
+    echo -n "$hostname"
+    set_color $filebase
+    echo -n " "(basename $PWD)" "
+    set_color $arrowcol -b normal
+    echo -n "⮀ "
 end
