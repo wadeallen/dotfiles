db2b683219d6f516e8fd6bb002fee397b5b5e46d
Wade Allen <wadesallen@gmail.com>
2020-09-03 11:32:55 -0400

    updated site

--- bin/choose_calendar.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-DATE=`zenity --calendar --date-format='%A, %B %d' 2>/dev/null`
-echo $DATE
--- bin/choose_calendar2.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-DATE=`zenity --calendar --date-format='%Y-%m-%d' 2>/dev/null`
-printf $DATE
