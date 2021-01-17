2e3f949234cbdb868e1b5f6d38c215c15705786c
Wade Allen <wadesallen@gmail.com>
2021-01-17 17:04:01 -0500

    updated site

--- Albert_Extensions/todoist.py
+++ Albert_Extensions/todoist.py
@@ -23,6 +23,8 @@ __authors__ = "Wade Allen"
 
 iconPath = os.path.dirname(__file__)+"/todoist.svg"
 
+
+
 def handleQuery(query):
     if not query.isTriggered:
         return
@@ -36,7 +38,10 @@ def handleQuery(query):
         return Item(id=__title__,
                     icon=iconPath,
                     text="Add Task: " + query.string,
-                    subtext="Type Task (Don't forget to include date")
+                    subtext="Type Task (Don't forget to include date)",
+                    actions=[
+                    	FuncAction("Add Todo", lambda: os.system(f"(/home/wadeallen/bin/todoist '{query.string}')&"))
+                    ])
 
 
 
