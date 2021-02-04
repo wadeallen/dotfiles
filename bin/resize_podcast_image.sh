5f9ed511860c04589a60ac9c15d74bd2d727d2bb
Wade Allen <wadesallen@gmail.com>
2021-02-04 15:15:49 -0500

    updated site

--- bin/resize_podcast_image.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-/usr/local/bin/mogrify -resize 420 -path $HOME/Sites/church_site/images/sermon $1
-cp /Users/wade/Pictures/fbc_podcast_logo.png $HOME/Desktop/fbc_podcast_logo.png
-open $HOME/Sites/church_site/images/sermon
