94fea3b391b04cee8f1262a849d06afd4f427da7
Wade Allen <wadesallen@gmail.com>
2021-03-28 21:58:43 -0400

    updated site

--- snippets/agenda.py
+++ snippets/agenda.py
@@ -1,6 +1,6 @@
 from datetime import date
-from dateutil.relativedelta import relativedelta
+from dateutil.relativedelta import relativedelta, TU
 today = date.today()
-tomorrow = today + relativedelta(days=1)
-meeting_date=tomorrow.strftime("%B %d")
+next_tuesday = today + relativedelta(weekday=TU(+1))
+meeting_date=next_tuesday.strftime("%a, %B %d")
 print(f"I have attached the agenda for the {meeting_date} meeting. Please let me know if you need to add additional items. See you tomorrow! ")
